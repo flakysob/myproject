9447d8aa04cca5fdd5b9ff76109aab3d419df379
flakysob <flakysob@gmail.com>
2023-10-04 17:26:18 +0300

    End of the Chapter 7

--- App.js
+++ App.js
@@ -1,53 +1,43 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-// https://www.youtube.com/watch?v=W-pg1r6-T0g&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=6
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+//https://www.youtube.com/watch?v=iMCM1NceGJY&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=7
 export default function App() {
 
   const [people, setPeople] = useState([
-    {name:'aaa', key:'1'},
-    {name:'bbb', key:'2'},
-    {name:'ccc', key:'3'},
-    {name:'ddd', key:'4'},
-    {name:'eee', key:'5'},
-    {name:'fff', key:'6'},
-    {name:'ggg', key:'7'},
-    {name:'hhh', key:'8'},
-    {name:'iii', key:'9'},
-    {name:'jjj', key:'10'},
-    {name:'aaa', key:'11'},
-    {name:'bbb', key:'12'},
-    {name:'ccc', key:'13'},
-    {name:'ddd', key:'14'},
-    {name:'eee', key:'15'},
-    {name:'fff', key:'16'},
-    {name:'ggg', key:'17'},
-    {name:'hhh', key:'18'},
-    {name:'iii', key:'19'},
-    {name:'jjj', key:'20'},
+    {name:'aaa',id:'1'},
+    {name:'bbb',id:'2'},
+    {name:'ccc',id:'3'},
+    {name:'ddd',id:'4'},
+    {name:'eee',id:'5'},
+    {name:'fff',id:'6'},
+    {name:'ggg',id:'7'},
+    {name:'hhh',id:'8'},
+    {name:'iii',id:'9'},
+    {name:'jjj',id:'10'},
+    {name:'aaa',id:'11'},
+    {name:'bbb',id:'12'},
+    {name:'ccc',id:'13'},
+    {name:'ddd',id:'14'},
+    {name:'eee',id:'15'},
+    {name:'fff',id:'16'},
+    {name:'ggg',id:'17'},
+    {name:'hhh',id:'18'},
+    {name:'iii',id:'19'},
+    {name:'jjj',id:'20'},
 
   ]);
 
   return (
     <View style={styles.container}>
       
-
-      {/* <ScrollView>
-      {people.map((item)=>{
-        return(
-          <View key={item.key}>
-            <Text style={styles.item}>{item.key} - {item.name}</Text>
-          </View>
-        )
-      })}
-      </ScrollView> */}
-      {/* These are doing exactly same thing!!!! */}
-      <ScrollView>
-      {people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.key} - {item.name}</Text>
-          </View>
-      ))}
-      </ScrollView>
+      <FlatList
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <Text style={styles.item}> {item.name} </Text>
+        )}
+      />
       
 
     </View>
@@ -64,6 +54,8 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
   item: {
+    marginTop: 24,
+    marginHorizontal: 10,
     marginTop: 24,
     padding: 30,
     backgroundColor: 'pink',
