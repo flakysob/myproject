dd50797d8e93e4eb4331dc3f20951bd163d17230
flakysob <flakysob@gmail.com>
2023-10-04 17:49:23 +0300

    End of the Chapter 8

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-//https://www.youtube.com/watch?v=iMCM1NceGJY&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=7
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+//https://www.youtube.com/watch?v=QhX25YGf8qg&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=8
+
 export default function App() {
 
   const [people, setPeople] = useState([
@@ -14,32 +15,38 @@ export default function App() {
     {name:'hhh',id:'8'},
     {name:'iii',id:'9'},
     {name:'jjj',id:'10'},
-    {name:'aaa',id:'11'},
-    {name:'bbb',id:'12'},
-    {name:'ccc',id:'13'},
-    {name:'ddd',id:'14'},
-    {name:'eee',id:'15'},
-    {name:'fff',id:'16'},
-    {name:'ggg',id:'17'},
-    {name:'hhh',id:'18'},
-    {name:'iii',id:'19'},
-    {name:'jjj',id:'20'},
+    {name:'kkk',id:'11'},
+    {name:'lll',id:'12'},
+    {name:'mmm',id:'13'},
+    {name:'nnn',id:'14'},
+    {name:'ooo',id:'15'},
+    {name:'ppp',id:'16'},
+    {name:'rrr',id:'17'},
+    {name:'sss',id:'18'},
+    {name:'ttt',id:'19'},
+    {name:'uuu',id:'20'},
 
   ]);
 
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+       return prevPeople.filter(person => person.id != id);
+      })
+  }
+
   return (
     <View style={styles.container}>
-      
       <FlatList
-        numColumns={2}
+        // numColumns={1}
         keyExtractor={(item) => item.id}
         data={people}
         renderItem={({ item }) => (
-          <Text style={styles.item}> {item.name} </Text>
+          <TouchableOpacity onPress={() => {pressHandler(item.id)}}>
+            <Text style={styles.item}> {item.name} </Text>
+          </TouchableOpacity>
         )}
       />
-      
-
     </View>
   );
 }
