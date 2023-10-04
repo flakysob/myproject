939bc94e9ed8b61b337811fd250126174e7adf63
flakysob <flakysob@gmail.com>
2023-10-04 14:22:16 +0300

    Learning React Native

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name,setName] = useState('John Doe');
+  const [age,setAge] = useState('30');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text style={styles.explainText}>Enter your name</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) => setName(val)}
+      />
+      <Text style={styles.explainText}>Enter your age</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g. 30'
+        onChangeText={(val) => setAge(val)}
+      />
+
+      <Text>Your name is {name}, age is {age}</Text>
     </View>
   );
 }
@@ -16,5 +33,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 2,
+    borderStyle: 'dotted',
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  explainText:{
+    fontWeight: 'bold',
+    fontSize: 20,
+    
   },
 });
